848b796cae37e19722cb79ebadb58f5d6030a3bf
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2023-12-08 11:27:27 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "Adarsh"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//conversion in number 
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn =0.1
+let booleanIsLoggedIn = Boolean(isLoggedIn)//conversion in Bollean
+// console.log(booleanIsLoggedIn)
+// 1 => true; 0 => false
+// "" => false
+// "Adarsh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//  console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
