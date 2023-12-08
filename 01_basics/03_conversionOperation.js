e3c55c6843917fb23a67dd5b4a2d20993566a03e
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2023-12-08 11:37:36 +0000

    operations on variables

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,43 @@ let stringNumber = String(someNumber)
 
 
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power 2^3
+// console.log(2/3);
+// console.log(2%3);
 
 
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2 //concate string
+// console.log(str3);
+
+// console.log("1" + 2); //string 12
+// console.log(1 + "2"); //string 12
+// console.log("1" + 2 + 2); /string 122
+// console.log(1 + 2 + "2"); /string 32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); //1
+// console.log(+""); //1
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// study of link
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
