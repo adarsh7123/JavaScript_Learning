e49397d6424b7b4dd148de5c349fea282598ffdf
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2023-12-08 09:40:49 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer Version
+
+// Alert(3+3) // used in browser 
+//node.js m nahi hota
+
+//always write redability code not like below one
+console.log(3+
+    3)
+
+let age = 18
+let Name = "Joe"
+let isStudent = true
+let state;
+
+console.log(typeof state) //undefined
+// console.table(typeof[age,Name,isStudent,state]) //object
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
