1716651ff0f75c93620bcf942b26d3279b9547fd
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2023-12-08 11:42:40 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+//normal comparision
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+//ulte fulte  comparision jo js m allowed h but typeScript m nahi
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true null become 0 first then comparision happens
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+// === 
+
+console.log("2" === 2);//false
