a0bf931c1d3ff4c59c411770f453410cc326838d
Verma Adarshkumar Dharmendrakumar <111121772+adarsh7123@users.noreply.github.com>
2023-12-11 09:51:20 +0000

    while , do while loop working in js

---
+++ 05_iterations/2.js
@@ -0,0 +1,21 @@
+let index =0;
+while (index<=10) {
+    // console.log(`value of index is ${index}`)
+    index += 2
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+let score = 12
+
+do {
+    console.log(`score is ${score}`)
+    score+=2
+}while(score<=20)
